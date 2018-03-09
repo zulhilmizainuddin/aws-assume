2473f005b0c6e4367e397d5845670c5c62f0773d
zulhilmizainuddin <zulhilmi.zainuddin@outlook.com>
2018-03-10 07:24:03 +0800

    Only support python 3.6

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
-setup(name='awsassumerole',
+setup(name='awsassume',
       version='0.1.0',
       description='Execute AWS command after assuming role',
       classifiers=[
@@ -8,16 +8,14 @@ setup(name='awsassumerole',
         'Intended Audience :: Developers',
         'Intended Audience :: System Administrators',
         'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Topic :: Utilities'
       ],
-      url='https://github.com/zulhilmizainuddin/aws-assumerole',
+      url='https://github.com/zulhilmizainuddin/aws-assume',
       author='Zulhilmi Mohamed Zainuddin',
       author_email='zulhilmi.zainuddin@outlook.com',
       license='MIT',
-      packages=['awsassumerole'],
+      packages=['awsassume'],
       install_requires=[
         'boto3'
       ],
