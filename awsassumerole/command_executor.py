c689ea1748d3a334ce64c8489cfdb04188c725a2
zulhilmizainuddin <zulhilmi.zainuddin@outlook.com>
2018-03-09 21:53:52 +0800

    Add type to command_executor

--- awsassumerole/command_executor.py
+++ awsassumerole/command_executor.py
@@ -1,6 +1,8 @@
 import subprocess
 
+from typing import Dict
+
 
 class CommandExecutor(object):
-    def execute(self, command, env_var):
+    def execute(self, command: str, env_var: Dict[str, str]) -> None:
         subprocess.Popen(command.split(' '), env=env_var)
