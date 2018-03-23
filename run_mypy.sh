b89b88e6f2e000a7714dc65e65fbe472e57eca4d
zulhilmizainuddin <zulhilmi.zainuddin@outlook.com>
2018-03-23 20:05:35 +0800

    Update mypy script

--- run_mypy.sh
+++ run_mypy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-export PYTHONPATH="$PYTHONPATH:$(pwd)/awsassume:$(pwd)/awsassume/tests"
+export PYTHONPATH="$PYTHONPATH:$(pwd)/awsassume"
 
 python3 -m mypy --ignore-missing-imports awsassume
