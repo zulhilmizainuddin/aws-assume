1e081f1954604c081c59f45ba9b326f5068aaffd
zulhilmizainuddin <zulhilmi.zainuddin@outlook.com>
2018-03-25 13:27:09 +0800

    Add AssumeRole unit test

---
+++ tests/test_assume_role.py
@@ -0,0 +1,26 @@
+import pytest
+
+from assume_role import AssumeRole
+from data_models import AssumeRoleArgs
+from unittest.mock import MagicMock
+
+
+@pytest.fixture(scope='module')
+def assume_role():
+    assume_role_args = AssumeRoleArgs(role_arn='arn:aws:iam::123456789012:role/rolename',
+                                      role_session_name='sessionname')
+
+    return AssumeRole(assume_role_args)
+
+
+@pytest.fixture(scope='module')
+def assumed_role_response():
+    from fixture.assumed_role_response import assumed_role_response
+
+    return assumed_role_response
+
+
+def test_assume_role(assume_role, assumed_role_response):
+    assume_role.client.assume_role = MagicMock(return_value=assumed_role_response)
+
+    assert assume_role.assume_role() == assumed_role_response
