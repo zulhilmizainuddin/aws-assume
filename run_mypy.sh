61814989faf8471509b68dca90e5de6e2e7a9f91
zulhilmizainuddin <zulhilmi.zainuddin@outlook.com>
2018-03-29 10:37:35 +0800

    Removed unit test and mypy scripts

--- run_mypy.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-export PYTHONPATH="$PYTHONPATH:$(pwd)/awsassume"
-
-python3 -m mypy --ignore-missing-imports awsassume
--- run_unit_test.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-export PYTHONPATH="$PYTHONPATH:$(pwd)/awsassume:$(pwd)/tests"
-
-#python3.6 -m unittest discover
-python3.6 -m pytest -vv
