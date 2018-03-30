d2f1cd87c3dae870319dc9810e2aa711996a197f
zulhilmizainuddin <zulhilmi.zainuddin@outlook.com>
2018-03-30 21:43:58 +0800

    Wrap main function with try except

--- awsassume/main.py
+++ awsassume/main.py
@@ -1,3 +1,5 @@
+import sys
+
 from awsassume.assume_role_executor import AssumeRoleExecutor
 from awsassume.assume_role_executor_factory import AssumeRoleExecutorFactory
 from awsassume.command_executor import CommandExecutor
@@ -7,23 +9,26 @@ from awsassume.environment_variable import EnvironmentVariable
 
 
 def main():
-    cli_args: CliArgs = CommandLineArgs().get_cli_args()
+    try:
+        cli_args: CliArgs = CommandLineArgs().get_cli_args()
 
-    assume_role_executor: AssumeRoleExecutor = AssumeRoleExecutorFactory.get_executor(cli_args)
+        assume_role_executor: AssumeRoleExecutor = AssumeRoleExecutorFactory.get_executor(cli_args)
 
-    credentials: Credentials = assume_role_executor.execute()
+        credentials: Credentials = assume_role_executor.execute()
 
-    if cli_args.command is None:
-        print('# AWS assumed role credentials:')
-        EnvironmentVariable.display_credentials_to_be_exported(credentials, cli_args.region_name)
+        if cli_args.command is None:
+            print('# AWS assumed role credentials:')
+            EnvironmentVariable.display_credentials_to_be_exported(credentials, cli_args.region_name)
 
-        print()
+            print()
 
-        print('# Execute the following command to export the AWS assumed role credentials to environment variables.')
+            print('# Execute the following command to export the AWS assumed role credentials to environment variables.')
 
-        command_to_eval = EnvironmentVariable.generate_command_to_export_credentials(credentials, cli_args.region_name)
-        print(command_to_eval)
-    else:
-        env_var_with_credentials = EnvironmentVariable.get_env_var_with_credentials(credentials, cli_args.region_name)
+            command_to_eval = EnvironmentVariable.generate_command_to_export_credentials(credentials, cli_args.region_name)
+            print(command_to_eval)
+        else:
+            env_var_with_credentials = EnvironmentVariable.get_env_var_with_credentials(credentials, cli_args.region_name)
 
-        CommandExecutor.execute(cli_args.command, env_var_with_credentials)
+            CommandExecutor.execute(cli_args.command, env_var_with_credentials)
+    except Exception as ex:
+        sys.exit(ex)
